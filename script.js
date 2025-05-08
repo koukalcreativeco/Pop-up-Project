6c64bce43f394fccc9b95a4716622a0715d63554
AUSTIN KOUKAL <au-s@live.com>
2025-05-08 09:30:23 -0700

    Finished

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cookie = document.getElementById("cookie");
+    cookie.style.display = "block";
+})
+
+const accept = () => {
+    const cookie = document.getElementById("cookie");
+    cookie.style.display = "none";
+}
